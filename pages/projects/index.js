5ab37d6f4159a734daa8ccf72710ade64702e87f
Hossam ELMansy <hossamelmansy.developer@gmail.com>
2021-02-19 13:24:27 +0200

    Added project (list) page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,18 +1,88 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
 
+import { useRouter } from 'next/router';
+
+import { Box, Button, Container, makeStyles, SvgIcon } from '@material-ui/core';
+
+import { PlusCircle as PlusCircleIcon } from 'react-feather';
+
+import Header from 'src/components/common/Header';
 import Page from 'src/components/common/Page';
 import Protected from 'src/components/common/Protected';
+import Results from 'src/components/pages/projects/Results';
+import useIsMountedRef from 'src/hooks/useIsMountedRef';
 import DashboardLayout from 'src/layouts/DashboardLayout';
 
-function ProjectsnewPage() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+}));
+
+const headerLinks = [
+  { title: 'Dashboard', href: '/dashboard' },
+  { title: 'Management', href: '#' },
+  { title: 'Projects' },
+];
+
+const PROJECTS = [];
+
+function ProjectsPage() {
+  const classes = useStyles();
+  const router = useRouter();
+  const isMountedRef = useIsMountedRef();
+  const [projects, setProjects] = React.useState([]);
+
+  const getProjects = React.useCallback(async () => {
+    try {
+      const data = { projects: PROJECTS };
+
+      if (isMountedRef.current) {
+        setProjects(data.projects);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, [isMountedRef]);
+
+  React.useEffect(() => {
+    getProjects();
+  }, [getProjects]);
+
   return (
-    <Page>
-      <h1>List all projects here </h1>
+    <Page className={classes.root} title="Projects - All">
+      <Container maxWidth={false}>
+        <Header
+          links={headerLinks}
+          mainText="All Projects"
+          rightComponent={
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={() => router.push('/projects/create')}
+              className={classes.action}
+              startIcon={
+                <SvgIcon fontSize="small">
+                  <PlusCircleIcon />
+                </SvgIcon>
+              }>
+              New Project
+            </Button>
+          }
+        />
+
+        <Box mt={3}>
+          <Results query="" />
+        </Box>
+      </Container>
     </Page>
   );
 }
-ProjectsnewPage.Guard = Protected;
-ProjectsnewPage.Layout = DashboardLayout;
 
-export default ProjectsnewPage;
+ProjectsPage.Guard = Protected;
+ProjectsPage.Layout = DashboardLayout;
+
+export default ProjectsPage;
---
+++ src/components/pages/projects/Results.js
@@ -0,0 +1,246 @@
+import React from 'react';
+
+import {
+  Box,
+  Card,
+  Checkbox,
+  CircularProgress,
+  Divider,
+  IconButton,
+  InputAdornment,
+  makeStyles,
+  SvgIcon,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TextField,
+} from '@material-ui/core';
+
+import clsx from 'clsx';
+import { useSnackbar } from 'notistack';
+import {
+  ArrowRight as ArrowRightIcon,
+  Edit as EditIcon,
+  Search as SearchIcon,
+} from 'react-feather';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+
+import Label from 'src/components/common/Label';
+import Link from 'src/components/common/Link';
+import useTable from 'src/hooks/useTable';
+
+import BulkOperations from './BulkOperations';
+
+const useStyles = makeStyles(() => ({
+  root: {},
+}));
+
+const getStatusLabel = (projectStatus) => {
+  const map = {
+    negotiation: {
+      text: 'Negotiation',
+      color: 'error',
+    },
+    signed: {
+      text: 'Signed',
+      color: 'secondary',
+    },
+    inprogress: {
+      text: 'InProgress',
+      color: 'warning',
+    },
+    finished: {
+      text: 'Finished',
+      color: 'success',
+    },
+  };
+
+  const { text, color } = map[projectStatus];
+
+  return <Label color={color}>{text}</Label>;
+};
+
+function Results({ className, query, ...rest }) {
+  const classes = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+  const [isBulkLoading, setIsBulkLoading] = React.useState(false);
+  const {
+    // items,
+    selectedItems,
+    isAllItemsSelected,
+    isSomeItemsSelected,
+    page,
+    limit,
+    hasNext,
+    hasPrev,
+    isLoading: isTableLoading,
+    setItems,
+    setSelectedItems,
+    enableBulkOperations,
+    handleSelectAllItems,
+    handleSelectOneItem,
+    handlePageChange,
+    handleLimitChange,
+  } = useTable({ query });
+
+  const items = [
+    {
+      id: 1,
+      projectName: 'Project 1',
+      customerName: 'Mohamed Hossam',
+      status: 'negotiation',
+      startDate: '01-01-2021',
+      endDate: '01-01-2021',
+    },
+    {
+      id: 1,
+      projectName: 'Project 2',
+      customerName: 'Mohamed Hossam',
+      status: 'signed',
+      startDate: '01-01-2021',
+      endDate: '01-01-2021',
+    },
+    {
+      id: 1,
+      projectName: 'Project 3',
+      customerName: 'Mohamed Hossam',
+      status: 'inprogress',
+      startDate: '01-01-2021',
+      endDate: '01-01-2021',
+    },
+    {
+      id: 1,
+      projectName: 'Project 4',
+      customerName: 'Mohamed Hossam',
+      status: 'finished',
+      startDate: '01-01-2021',
+      endDate: '01-01-2021',
+    },
+  ];
+
+  return (
+    <div className={clsx(classes.root, className)} {...rest}>
+      <Card>
+        <Box p={2}>
+          <TextField
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SvgIcon fontSize="small" color="action">
+                    <SearchIcon />
+                  </SvgIcon>
+                </InputAdornment>
+              ),
+            }}
+            style={{ width: '25%' }}
+            onChange={() => {}}
+            placeholder="Search Projects"
+            value=""
+            variant="outlined"
+          />
+        </Box>
+        <Divider />
+        <PerfectScrollbar>
+          <Box minWidth={700}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell padding="checkbox">
+                    <Checkbox
+                      checked={isAllItemsSelected}
+                      indeterminate={isSomeItemsSelected}
+                      onChange={handleSelectAllItems}
+                    />
+                  </TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Customer</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Start Date</TableCell>
+                  <TableCell>End Date</TableCell>
+                  <TableCell align="right">Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              {isTableLoading ? (
+                <Box display="flex" justifyContent="center" mt={4}>
+                  <CircularProgress />
+                </Box>
+              ) : (
+                <TableBody>
+                  {items.map((item) => {
+                    const isItemSelected = selectedItems.includes(item.id);
+
+                    return (
+                      <TableRow
+                        hover
+                        key={item.id}
+                        selected={selectedItems.indexOf(item.id) !== -1}>
+                        <TableCell padding="checkbox">
+                          <Checkbox
+                            checked={isItemSelected}
+                            onChange={(event) =>
+                              handleSelectOneItem(event, item.id)
+                            }
+                            value={isItemSelected}
+                          />
+                        </TableCell>
+                        <TableCell>{item.projectName}</TableCell>
+                        <TableCell>{item.customerName}</TableCell>
+                        <TableCell>{getStatusLabel(item.status)}</TableCell>
+                        <TableCell>{item.startDate}</TableCell>
+                        <TableCell>{item.endDate}</TableCell>
+                        <TableCell align="right">
+                          <IconButton
+                            component={Link}
+                            href={`/projects/${item.id}/edit`}>
+                            <SvgIcon fontSize="small">
+                              <EditIcon />
+                            </SvgIcon>
+                          </IconButton>
+                          <IconButton
+                            component={Link}
+                            href={`/projects/${item.id}`}>
+                            <SvgIcon fontSize="small">
+                              <ArrowRightIcon />
+                            </SvgIcon>
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              )}
+            </Table>
+          </Box>
+        </PerfectScrollbar>
+        <TablePagination
+          component="div"
+          count={-1}
+          onChangePage={handlePageChange}
+          onChangeRowsPerPage={handleLimitChange}
+          page={page}
+          rowsPerPage={limit}
+          rowsPerPageOptions={[5, 10, 25]}
+          nextIconButtonProps={{ disabled: !hasNext }}
+          backIconButtonProps={{ disabled: !hasPrev }}
+          labelDisplayedRows={({ from }) => {
+            if (items.length == 0) return '0-0';
+            return `${from}-${from + items.length - 1}`;
+          }}
+        />
+      </Card>
+      <BulkOperations
+        open={enableBulkOperations}
+        selected={selectedItems}
+        isLoading={isBulkLoading}
+        onMarkActive={() => {}}
+        onMarkInactive={() => {}}
+        onDelete={() => {}}
+      />
+    </div>
+  );
+}
+
+export default Results;
